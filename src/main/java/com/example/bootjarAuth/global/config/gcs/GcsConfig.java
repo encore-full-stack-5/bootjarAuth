bc383505cf5b054c4aa843dcc5a72fc2d3928382
jungryuel <zldrktduf0927@naver.com>
2024-06-26 19:52:28 +0900

    [Test] CICD test

--- src/main/java/com/example/bootjarAuth/global/config/gcs/GcsConfig.java
+++ src/main/java/com/example/bootjarAuth/global/config/gcs/GcsConfig.java
@@ -7,9 +7,10 @@ import com.google.cloud.storage.StorageOptions;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
 
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 import javax.annotation.PostConstruct;
 
@@ -30,9 +31,9 @@ public class GcsConfig {
 
     @Bean
     public Storage storage() throws IOException {
-        ClassPathResource resource = new ClassPathResource(credentialsFileName);
-        System.out.println(resource.getFilename());
-        GoogleCredentials credentials = GoogleCredentials.fromStream(resource.getInputStream());
+//        ClassPathResource resource = new ClassPathResource(credentialsFileName);
+        InputStream resource = new FileInputStream(credentialsFileName);
+        GoogleCredentials credentials = GoogleCredentials.fromStream(resource);
         return StorageOptions.newBuilder()
                 .setProjectId(projectId)
                 .setCredentials(credentials)
